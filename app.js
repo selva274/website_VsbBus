a77721747a344c3edb79487018b1040949bfbd05
skcreationz <selvakumard274@gmail,com>
2023-05-13 17:27:55 +0530

    facebook updation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const ejs=require('ejs');
 const bodyParser=require('body-parser');
 const cookieSession=require('cookie-session');
+const facebookStrategy=require('passport-facebook').Strategy;
 const passport=require('passport')
 
 const app=express()
@@ -16,7 +17,7 @@ app.get("/",(req,res)=>{
 })
 
 
-//google sign-IN
+//google sign-IN////////////////////////////////////////////////////////////////////////////////////////////
 app.use(cookieSession({
 	name: 'google-auth-session',
 	keys: ['key1', 'key2']
@@ -49,7 +50,56 @@ app.get('/failure' , (req , res) => {
 	res.send("<h1>404 Error</h1>");
 })
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////facebook sign-IN///////////////////////////////////////////////////////////
+passport.use(new facebookStrategy({
+    clientID:"1429071757631314",
+    clientSecret:"2d652ff2bbfbe26779ba8216ab9fa4fb",
+    callbackURL:"https://successful-frog-gilet.cyclic.app/facebook/callback",
+    profileFields:['id','displayName','name','gender','picture.type(large)','email']
+},function(token,refresh_token,profile,done){
+console.log(profile);
+return done(null,profile)
+}))
 
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next()
+    }else{
+        res.redirect('/')
+    }
+}
+
+
+
+
+
+app.get('/auth/facebook',passport.authenticate('facebook',{scope:'email'}))
+app.get('/facebook/callback',passport.authenticate('facebook',{successRedirect:"/profile",failureRedirect:'/error'}))
+
+app.get("/profile",isLoggedIn,(req,res)=>{
+    res.redirect('/dashboard');
+});
+app.get("/error",(req,res)=>{
+    res.send('<h1>error to login</h1>')
+})
+
+
+
+passport.serializeUser(function(user,done){
+    done(null,user);
+})
+passport.deserializeUser(function(id,done){
+   
+    return done(null,id);
+})
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.post("/dashboard",(req,res)=>{
     res.send("<h1 style='text-align:center'>Welcome to Our website</h1>")
