84f52d4325101604105c9a4dadc6b49b61a3cc05
skcreationz <selvakumard274@gmail,com>
2023-05-13 16:22:37 +0530

    uri updation

--- passport-setup.js
+++ passport-setup.js
@@ -7,7 +7,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
 	clientID:"516180371370-7ia2tppunnolnd137ra5afktmq3s9951.apps.googleusercontent.com", // Your Credentials here.
 	clientSecret:"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", // Your Credentials here.
-	callbackURL:"https://rich-cyan-slug-veil.cyclic.app/google/callback",
+	callbackURL:"https://successful-frog-gilet.cyclic.app/google/callback",
 	passReqToCallback:true
 },
 function(request, accessToken, refreshToken, profile, done) {
