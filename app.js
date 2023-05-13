c16386ec93af679bcbc124a024fda436a1ac510e
skcreationz <selvakumard274@gmail,com>
2023-05-13 16:08:44 +0530

    updation login page

--- app.js
+++ app.js
@@ -1,8 +1,63 @@
 const express=require('express');
+const ejs=require('ejs');
+const cookieSession=require('cookie-session');
+const passport=require('passport')
+
 const app=express()
+require('./passport-setup');
+app.set('view engine','ejs');
+app.use(express.static('stylesheet'))
 
 app.get("/",(req,res)=>{
-    res.send("Hello Guys")
+    res.render('login')
+})
+
+
+//google sign-IN
+app.use(cookieSession({
+	name: 'google-auth-session',
+	keys: ['key1', 'key2']
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Auth
+app.get('/google' , passport.authenticate('google', { scope:
+	[ 'profile', 'email' ]
+}));
+
+// Auth Callback
+app.get( '/google/callback',
+	passport.authenticate( 'google', {
+		
+		failureRedirect: '/failure'}),function (req,res) {
+			res.redirect("/success")
+		}
+);
+
+// Success
+app.get('/success' , (req , res) => {
+
+	
+	async function run(){
+		const newuser=new GoogleModel({email:`${req.user.email}`});
+		await newuser.save();
+	}
+    run();
+
+	res.redirect("/dashboard")
+});
+
+// failure
+app.get('/failure' , (req , res) => {
+	res.send("<h1>404 Error</h1>");
+})
+
+
+
+app.post("/dashboard",(req,res)=>{
+    res.send("<h1 style='text-align:center'>Welcome to Our website</h1>")
 })
 
 app.listen(3000,()=>{
---
+++ passport-setup.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+
+
+
+passport.use(new GoogleStrategy({
+	clientID:"516180371370-7ia2tppunnolnd137ra5afktmq3s9951.apps.googleusercontent.com", // Your Credentials here.
+	clientSecret:"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", // Your Credentials here.
+	callbackURL:"https://rich-cyan-slug-veil.cyclic.app/google/callback",
+	passReqToCallback:true
+},
+function(request, accessToken, refreshToken, profile, done) {
+	// console.log(profile)
+	return done(null, profile);
+}
+));
+passport.serializeUser(function(user , done){
+	done(null , user);
+})
+passport.deserializeUser(function(user, done){
+	
+	done(null, user);
+});
