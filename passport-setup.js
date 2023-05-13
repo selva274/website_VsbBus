6931ecd15a2760648368178f6859ba3edf2cdd9d
skcreationz <selvakumard274@gmail,com>
2023-05-13 16:51:14 +0530

    cloude updation

--- passport-setup.js
+++ passport-setup.js
@@ -5,8 +5,8 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 
 passport.use(new GoogleStrategy({
-	clientID:"516180371370-7ia2tppunnolnd137ra5afktmq3s9951.apps.googleusercontent.com", // Your Credentials here.
-	clientSecret:"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", // Your Credentials here.
+	clientID:"516180371370-1li454tcudhg1m573mnsjl77h48i4uov.apps.googleusercontent.com", // Your Credentials here.
+	clientSecret:"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", // Your Credentials here.
 	callbackURL:"https://successful-frog-gilet.cyclic.app/google/callback",
 	passReqToCallback:true
 },
