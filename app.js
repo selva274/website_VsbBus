acceb291da7ffe91f95ec9fbcee60c662a7643e4
skcreationz <selvakumard274@gmail,com>
2023-05-13 16:27:37 +0530

    last updation

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express=require('express');
 const ejs=require('ejs');
+const bodyParser=require('body-parser');
 const cookieSession=require('cookie-session');
 const passport=require('passport')
 
 const app=express()
 require('./passport-setup');
+app.use(express.static('stylesheet'));
 app.set('view engine','ejs');
-app.use(express.static('stylesheet'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}))
 
 app.get("/",(req,res)=>{
     res.render('login')
